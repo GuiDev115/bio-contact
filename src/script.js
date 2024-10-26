7d9ca3eb69d3ecdd0ab3300255632e4b103a83aa
guidev <guilherme.fabricio.115@gmail.com>
2024-10-26 16:50:41 -0300

    :sparkles: responsividade para dispositivos mobile concluida WIP - version 1.0 feita

---
+++ src/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const menuItems = document.querySelector('.menu-items');
+
+    hamburger.addEventListener('click', function() {
+        menuItems.classList.toggle('active');
+    });
+
+    // Fechar o menu ao clicar em um link
+    const menuLinks = document.querySelectorAll('.menu-items a');
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            menuItems.classList.remove('active');
+        });
+    });
+
+    // Fechar o menu ao clicar fora dele
+    document.addEventListener('click', function(event) {
+        if (!hamburger.contains(event.target) && !menuItems.contains(event.target)) {
+            menuItems.classList.remove('active');
+        }
+    });
+
+    const blogLink = document.querySelector('.blog-link');
+    if (blogLink) {
+        blogLink.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevenir o comportamento padrão do link
+            alert('Ò blog está em construção!');
+        });
+    }
+});
+
